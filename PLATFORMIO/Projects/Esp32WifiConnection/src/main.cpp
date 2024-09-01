00d12c42518cc38a0b3973afcf85f7b1cfb8ae23
luoxianwu <xianwu2001@yahoo.com>
2024-08-31 23:18:26 -0600

    scan wifi, show ip
    console output:
    WiFi in STATION mode
    .8 network(s) found
    network 1: TP-Link_A6B0 (-21) ***
    network 2: MajeNet (-71) ***
    network 3: NETGEAR75 (-79) ***
    network 4: MajeNet_2 (-82) ***
    network 5: smileynet_IoT (-82) ***
    network 6: smileynet_IoT (-88) ***
    network 7: CenturyLink8212 (-88) ***
    network 8: Comiskey guest (-94) ***
    Connect to TP-Link_A6B0
    Connecting...
    WiFi connected. IP: 192.168.0.140

--- PLATFORMIO/Projects/Esp32WifiConnection/src/main.cpp
+++ PLATFORMIO/Projects/Esp32WifiConnection/src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <WiFiMulti.h>
+#include <WiFi.h>
+
 
 #define WIFI_SSID "TP-Link_A6B0" // Ensure you connect to the 2.4 GHz network , 5G won't work  
 #define WIFI_PSWD "Wi%416%Fi"
@@ -10,15 +12,42 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
 
-  Serial.println("add access point");
+  Serial.println("WiFi in STATION mode");
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(1000);
+
+  while(1){
+    Serial.print(".");
+    int n = WiFi.scanNetworks();
+    if(n!=0){
+        Serial.print(n); Serial.println(" network(s) found");
+        for(int i=0; i<n; i++){
+            Serial.print("network "); Serial.print(i + 1); Serial.print(": ");
+            Serial.print(WiFi.SSID(i));
+            Serial.print(" ("); Serial.print(WiFi.RSSI(i)); Serial.print(")");
+            Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)? " open ":" ***");
+        }
+        break;
+    }
+    else {
+      Serial.println(" No network found");
+    }
+  }
+
+
+  Serial.println("Connect to " WIFI_SSID);
   wifiMulti.addAP( WIFI_SSID, WIFI_PSWD );
   Serial.println("Connecting...");
 
   while(wifiMulti.run() != WL_CONNECTED) {
     Serial.println("[WIFI] Not connected!");
     delay(500);
+    Serial.print(".");
   }
-    Serial.println("WiFi connected");
+  Serial.print("WiFi connected. IP: ");
+  Serial.println(WiFi.localIP());
+
 }
 
 void loop() {
