61a2c0a3e718e97942e47c66742126bc7583e0a0
xian <xianwu2001@yahoo.com>
2024-09-10 17:40:35 -0600

    improve web page

--- PLATFORMIO/Projects/Esp32WifiConnection/src/webserver.cpp
+++ PLATFORMIO/Projects/Esp32WifiConnection/src/webserver.cpp
@@ -74,14 +74,27 @@ String getHTML() {
     htmlcode += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
     htmlcode += "<title>LED Control</title>\n";
     
+    htmlcode += "<style>html { font-family: Open Sans; display: inline-block; margin: 0px auto; text-align: center;}\n";
+    htmlcode += "body{margin-top: 100px; display: flex; flex-direction: column; align-items: center;}\n";  /* Updated line */
+    htmlcode += "h1 {color: #154c79; margin: 30px auto 30px;} h3 {color: #1e81b0; margin-bottom: 50px;}\n";
+    htmlcode += ".button {display: block; width: 150px; background-color: #3498db; border: none; color: white; padding: 13px 30px; text-decoration: none; margin: 10px auto;}\n"; /* Added margin for better centering */
+    htmlcode += ".button-on {background-color: #1e81b0;}\n";
+    htmlcode += ".button-on:active {background-color: #15767b;}\n";
+    htmlcode += ".button-off {background-color: #5e0f1a;}\n";
+    htmlcode += ".button-off:active {background-color: #4b0c15;}\n";
+    htmlcode += "p {font-size: 18px; color: #888; margin-bottom: 10px;}\n";
+    htmlcode += "</style>\n";
+    
+
+
     htmlcode += "</head>\n";
     htmlcode += "<body>\n";
     htmlcode += "<h1>ESP32 Web Server</h1>\n";
     htmlcode += "<h3>A simple demo using Access Point (AP) Mode</h3>\n";
     if (LEDStatus) {
-        htmlcode += "<p>Blue LED Status: ON</p><p><a href=\"/ledoff\">Turn it OFF</a></p>\n";
+        htmlcode += "<p>Blue LED Status: ON</p><p><a class=\"button button-off\" href=\"/ledoff\">Turn it OFF</a></p>\n";
     } else {
-        htmlcode += "<p>Blue LED Status: OFF</p><p><a href=\"/ledon\">Turn it ON</a></p>\n";
+        htmlcode += "<p>Blue LED Status: OFF</p><p><a class=\"button button-on\" href=\"/ledon\">Turn it ON</a></p>\n";
     }
     htmlcode += "</body>\n";
     htmlcode += "</html>\n";
